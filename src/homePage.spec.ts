4c1cdee74b888b0c1b11ae610e830659b88c3ef3
Tatiana Golinska <tatiana.golinska@gmail.com>
2024-04-22 09:41:02 +0200

    test: added main pages smokes

--- src/homePage.spec.ts
+++
@@ -1,9 +0,0 @@
-import { BasePage } from './base.page';
-import { Page } from '@playwright/test';
-
-export class HomePage extends BasePage {
-  url = '/';
-  constructor(page: Page) {
-    super(page);
-  }
-}
--- tests/webpage.spec.ts
+++ tests/webpage.spec.ts
@@ -1,12 +1,47 @@
-import { HomePage } from '../src/homePage.spec';
+import { ArticlesPage } from '../../src/pages/articles.page';
+import { CommentsPage } from '../../src/pages/comments.page';
+import { HomePage } from '../../src/pages/home.page';
 import { expect, test } from '@playwright/test';
 
-test('home page title @TDI-R01-01', async ({ page }) => {
-  //Arrange
-  const homePage = new HomePage(page);
-  //Act
-  await homePage.goto();
-  // await this.page.waitForLoadState();
-  const title = await homePage.title();
-  expect(title).toContain('Todoist');
+test.describe('Verify service main pages', () => {
+  test('home page title', async ({ page }) => {
+    // Arrange
+    const homePage = new HomePage(page);
+
+    // Act
+    await homePage.goto();
+
+    // Assert
+    const title = await homePage.title();
+    expect(title).toContain('GAD');
+  });
+
+  test('articles page title', async ({ page }) => {
+    // Arrange
+    const articlesPage = new ArticlesPage(page);
+
+    // Act
+    await articlesPage.goto();
+
+    // Assert
+    const title = await articlesPage.title();
+    expect(title).toContain('Articles');
+  });
+
+  test('comments page title', async ({ page }) => {
+    // Arrange
+    const commentsPage = new CommentsPage(page);
+
+    // Act
+    await commentsPage.goto();
+
+    // Assert
+    const title = await commentsPage.title();
+    expect(title).toContain('Comments');
+  });
+
+  test('home page title simple', async ({ page }) => {
+    await page.goto('');
+    await expect(page).toHaveTitle(/GAD/);
+  });
 });
