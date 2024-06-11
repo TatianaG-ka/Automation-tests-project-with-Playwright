ba69432b9532588c952cb32499ec0a0e914d501e
Tatiana Golinska <tatiana.golinska@gmail.com>
2024-06-11 10:11:46 +0200

    feat: added faker to tests

--- tests/register.spec.ts
+++ tests/register.spec.ts
@@ -1,22 +1,31 @@
 import { LoginPage } from '../src/pages/login.page';
 import { RegisterPage } from '../src/pages/register.page';
-import { expect, test } from '@playwright/test';
 import { WelcomePage } from '../src/pages/welcome.page';
+import { faker } from '@faker-js/faker';
+import { expect, test } from '@playwright/test';
 
 test.describe('Verify register', () => {
   test('Register with correct data and login @R03-01 @R03-02 @R03-03', async ({
     page,
   }) => {
     //Array
-    const firstName = 'Jan';
-    const lastName = 'JanTest';
-    const userEmail = `jan${new Date().getTime()}@test.test`;
-    const userPassword = 'Test1234';
+    const userFirstName = faker.person.firstName();
+    const userLastName = faker.person.lastName();
+    const userEmail = faker.internet.email({
+      firstName: userFirstName,
+      lastName: userLastName,
+    });
+    const userPassword = faker.internet.password();
     const registerPage = new RegisterPage(page);
 
     //Act
     await registerPage.goto();
-    await registerPage.register(firstName, lastName, userEmail, userPassword);
+    await registerPage.register(
+      userFirstName,
+      userLastName,
+      userEmail,
+      userPassword,
+    );
 
     const expectedAlertPopup = 'User created';
 
