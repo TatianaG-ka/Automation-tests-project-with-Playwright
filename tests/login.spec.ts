905ef97d5ff2a87d870b9c30ce34044eb7d80433
Tatiana Golinska <tatiana.golinska@gmail.com>
2024-06-11 14:24:14 +0200

    chore: login method refactor

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -7,16 +7,11 @@ import { expect, test } from '@playwright/test';
 test.describe('Verify login', () => {
   test('login with correct credentials @R02-01', async ({ page }) => {
     //Arrange
-    const loginUserData: LoginUser = {
-      userEmail: testUser1.userEmail,
-      userPassword: testUser1.userPassword,
-    };
-
     const loginPage = new LoginPage(page);
 
     //Act
     await loginPage.goto();
-    await loginPage.loginNew(loginUserData);
+    await loginPage.login(testUser1);
 
     const welcomePage = new WelcomePage(page);
     const title = await welcomePage.title();
@@ -27,13 +22,16 @@ test.describe('Verify login', () => {
 
   test('reject login with incorrect password @R02-01', async ({ page }) => {
     //Arrange
-    const userEmail = testUser1.userEmail;
-    const userPassword = 'incorrectPassword';
+    const loginUserData: LoginUser = {
+      userEmail: testUser1.userEmail,
+      userPassword: 'incorrectPassword',
+    };
+
     const loginPage = new LoginPage(page);
 
     //Act
     loginPage.goto();
-    loginPage.login(userEmail, userPassword);
+    loginPage.login(loginUserData);
     const errorMessage = loginPage.loginError;
 
     //Assert
