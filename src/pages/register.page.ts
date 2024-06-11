594b39f921812f37fe71ed26395344f6613759d1
Tatiana Golinska <tatiana.golinska@gmail.com>
2024-06-11 12:12:53 +0200

    chore: register user model

--- src/pages/register.page.ts
+++ src/pages/register.page.ts
@@ -1,3 +1,4 @@
+import { RegisterUser } from '../models/user.model';
 import { BasePage } from './base.page';
 import { Page } from '@playwright/test';
 
@@ -14,16 +15,11 @@ export class RegisterPage extends BasePage {
     super(page);
   }
 
-  async register(
-    firstName: string,
-    lastName: string,
-    email: string,
-    password: string,
-  ): Promise<void> {
-    await this.firstNameInput.fill(firstName);
-    await this.lastNameInput.fill(lastName);
-    await this.emailInput.fill(email);
-    await this.passwordInput.fill(password);
+  async register(registerUserData: RegisterUser): Promise<void> {
+    await this.firstNameInput.fill(registerUserData.userFirstName);
+    await this.lastNameInput.fill(registerUserData.userLastName);
+    await this.emailInput.fill(registerUserData.userEmail);
+    await this.passwordInput.fill(registerUserData.userPassword);
     await this.registerButton.click();
   }
 }
