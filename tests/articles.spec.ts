efbad89a973bf165c05385d5a3d51df5d61cce51
Tatiana Golinska <tatiana.golinska@gmail.com>
2024-06-14 11:11:15 +0200

    tests: negative tests to create article

--- tests/articles.spec.ts
+++ tests/articles.spec.ts
@@ -36,4 +36,46 @@ test.describe('Verify articles', () => {
       .soft(page.getByTestId('alert-popup'))
       .toHaveText('Article was created');
   });
+
+  test('reject creating article without title @R04-01', async ({ page }) => {
+    //Array
+    const loginPage = new LoginPage(page);
+    await loginPage.goto();
+    await loginPage.login(testUser1);
+    const articlesPage = new ArticlesPage(page);
+    await articlesPage.goto();
+
+    //Act
+    await articlesPage.addArticleButtonLogged.click();
+    const addArticleView = new AddArticleView(page);
+
+    const articleData = randomNewArticle();
+    articleData.title = '';
+    await addArticleView.createArticle(articleData);
+
+    //Assert
+    const expectedErrorMessage = 'Article was not created';
+    await expect(addArticleView.alertPopup).toHaveText(expectedErrorMessage);
+  });
+
+  test('rejected creating article without body @R04-01', async ({ page }) => {
+    //Arrange
+    const loginPage = new LoginPage(page);
+    const articlesPage = new ArticlesPage(page);
+    const addArticleView = new AddArticleView(page);
+    const articleData = randomNewArticle();
+    articleData.body = '';
+    const expectedErrorMessage = 'Article was not created';
+
+    await loginPage.goto();
+    await loginPage.login(testUser1);
+    await articlesPage.goto();
+    await articlesPage.addArticleButtonLogged.click();
+
+    //Act
+    await addArticleView.createArticle(articleData);
+
+    //Assert
+    await expect(addArticleView.alertPopup).toHaveText(expectedErrorMessage);
+  });
 });
