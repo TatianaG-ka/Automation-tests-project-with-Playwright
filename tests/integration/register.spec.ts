1fffa5139ce8df101ba96080f2c023b15345e5e2
Tatiana Golinska <tatiana.golinska@gmail.com>
2024-06-19 15:05:48 +0200

    chore: refactor register action to return po

--- tests/integration/register.spec.ts
+++ tests/integration/register.spec.ts
@@ -1,6 +1,5 @@
 import { prepareRandomUser } from '@_src/factories/user.factory';
 import { RegisterUserModel } from '@_src/models/user.model';
-import { LoginPage } from '@_src/pages/login.page';
 import { RegisterPage } from '@_src/pages/register.page';
 import { expect, test } from '@playwright/test';
 
@@ -15,27 +14,23 @@ test.describe('Verify register', () => {
     await registerPage.goto();
   });
 
-  test('Register with correct data and login @R03-01 @R03-02 @R03-03', async ({
-    page,
-  }) => {
+  test('Register with correct data and login @R03-01 @R03-02 @R03-03', async ({}) => {
     //Array
     const expectedAlertPopup = 'User created';
-
-    const loginPage = new LoginPage(page);
+    const expectedLoginTitle = 'Login';
+    const expectedWelcomeTitle = 'Welcome';
 
     //Act
-    await registerPage.register(registerUserData);
+    const loginPage = await registerPage.register(registerUserData);
 
     //Assert
     await expect(registerPage.alertPopup).toHaveText(expectedAlertPopup);
 
-    const expectedLoginTitle = 'Login';
     await loginPage.waitForPageToLoadUrl();
     const titleLogin = await loginPage.getTitle();
     expect.soft(titleLogin).toContain(expectedLoginTitle);
 
     //Assert test login
-    const expectedWelcomeTitle = 'Welcome';
     const welcomePage = await loginPage.login({
       userEmail: registerUserData.userEmail,
       userPassword: registerUserData.userPassword,
