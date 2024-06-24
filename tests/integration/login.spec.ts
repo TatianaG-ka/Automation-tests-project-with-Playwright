f71de527832431004978efdd226255462131657d
Tatiana Golinska <tatiana.golinska@gmail.com>
2024-06-24 10:24:39 +0200

    chore: own fixtures in login tests

--- tests/integration/login.spec.ts
+++ tests/integration/login.spec.ts
@@ -1,16 +1,13 @@
+import { expect, test } from '@_src/fixtures/merge.fixture';
 import { LoginUserModel } from '@_src/models/user.model';
-import { LoginPage } from '@_src/pages/login.page';
 import { testUser1 } from '@_src/test-data/user.data';
-import { expect, test } from '@playwright/test';
 
 test.describe('Verify login', () => {
-  test('login with correct credentials @R02-01', async ({ page }) => {
+  test('login with correct credentials @R02-01', async ({ loginPage }) => {
     //Arrange
     const expectedWelcomeTitle = 'Welcome';
-    const loginPage = new LoginPage(page);
 
     //Act
-    await loginPage.goto();
     const welcomePage = await loginPage.login(testUser1);
     await welcomePage.goto();
     const title = await welcomePage.getTitle();
@@ -19,10 +16,11 @@ test.describe('Verify login', () => {
     expect(title).toContain(expectedWelcomeTitle);
   });
 
-  test('reject login with incorrect password @R02-01', async ({ page }) => {
+  test('reject login with incorrect password @R02-01', async ({
+    loginPage,
+  }) => {
     //Arrange
     const expectedLoginTitle = 'Login';
-    const loginPage = new LoginPage(page);
 
     const LoginUserModelData: LoginUserModel = {
       userEmail: testUser1.userEmail,
@@ -30,7 +28,6 @@ test.describe('Verify login', () => {
     };
 
     //Act
-    loginPage.goto();
     loginPage.login(LoginUserModelData);
     const errorMessage = loginPage.loginError;
 
