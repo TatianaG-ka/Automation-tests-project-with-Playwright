1f57315939be0f0c18759e698c47848f4f2cdaac
Tatiana Golinska <tatiana.golinska@gmail.com>
2024-06-24 10:39:56 +0200

    chore: own fixtures in lifecycle tests

--- tests/end-to-end/article-lifecycle.spec.ts
+++ tests/end-to-end/article-lifecycle.spec.ts
@@ -1,26 +1,16 @@
 import { prepareRandomNewArticle } from '@_src/factories/article.factory';
+import { expect, test } from '@_src/fixtures/merge.fixture';
 import { AddArticleModel } from '@_src/models/article.model';
-import { ArticlesPage } from '@_src/pages/articles.page';
-import { expect, test } from '@playwright/test';
 
 test.describe.configure({ mode: 'serial' });
 test.describe('Create, verify and delete article', () => {
-  let articlesPage: ArticlesPage;
   let articleData: AddArticleModel;
 
-  test.beforeEach(async ({ page }) => {
-    articlesPage = new ArticlesPage(page);
-
-    await articlesPage.goto();
-  });
-
-  test('create new article @R04-01 @logged', async ({ page }) => {
+  test('create new article @R04-01 @logged', async ({ addArticleView }) => {
     //Arrange
     articleData = prepareRandomNewArticle();
 
     //Act
-    const addArticleView = await articlesPage.clickAddArticleButtonLogged();
-    await expect.soft(addArticleView.addNewHeader).toBeVisible();
     const articlePage = await addArticleView.createArticle(articleData);
 
     //Assert
@@ -28,13 +18,11 @@ test.describe('Create, verify and delete article', () => {
     await expect
       .soft(articlePage.articleBody)
       .toHaveText(articleData.body, { useInnerText: true });
-
-    await expect
-      .soft(page.getByTestId('alert-popup'))
-      .toHaveText('Article was created');
   });
 
-  test('user can access single article @R04-03 @logged', async () => {
+  test('user can access single article @R04-03 @logged', async ({
+    articlesPage,
+  }) => {
     //Act
     const articlePage = await articlesPage.gotoArticle(articleData.title);
 
@@ -45,7 +33,9 @@ test.describe('Create, verify and delete article', () => {
       .toHaveText(articleData.body, { useInnerText: true });
   });
 
-  test('user can delete his own article @R04-04 @logged', async () => {
+  test('user can delete his own article @R04-04 @logged', async ({
+    articlesPage,
+  }) => {
     //Arrange
     const expectedArticlesTitle = 'Articles';
     const expectedNoResultText = 'No data';
