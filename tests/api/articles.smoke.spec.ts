220f2cb903531d56843f890332caf1d6c4f04b1d
Tatiana Golinska <tatiana.golinska@gmail.com>
2024-07-14 19:55:37 +0200

    test: api smoke articles extended test

--- tests/api/articles.smoke.spec.ts
+++ tests/api/articles.smoke.spec.ts
@@ -1,7 +1,9 @@
 import test, { expect } from '@playwright/test';
 
 test.describe('Verify articles API endpoint', () => {
-  test('GET articles returns status code 200, @R07-01 @api', async ({ request }) => {
+  test('GET articles returns status code 200, @R07-01 @api', async ({
+    request,
+  }) => {
     //Array
     const expectedStatusCode = 200;
     const articleUrl = '/api/articles';
@@ -10,5 +12,22 @@ test.describe('Verify articles API endpoint', () => {
 
     //Assert
     expect(response.status()).toBe(expectedStatusCode);
+  });
+
+  test('Get articles should return at least one article @predefined_data', async ({
+    request,
+  }) => {
+    //Array
+    const expectedMinArticlesCount = 1;
+    const articlesUrl = '/api/articles';
+
+    //Act
+    const response = await request.get(articlesUrl);
+    const responseJason = await response.json();
+
+    //Assert
+    expect(responseJason.length).toBeGreaterThanOrEqual(
+      expectedMinArticlesCount,
+    );
   });
 });
